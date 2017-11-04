b6483fd489a7ec1816ef3ae1bfee1bf85a6a6aeb
DanDanTseng <DanDanTseng@users.noreply.github.com>
2017-11-04 21:55:22 +0800

    add command quote, weather v1

--- src/mainbot.js
+++ src/mainbot.js
@@ -1,5 +1,41 @@
+const format = require('date-fns/format');
 const TelegramBot = require('node-telegram-bot-api');
+
+const fetch = require('node-fetch');
 let bot;
+
+const getQuote=()=>{
+
+    const quoteAPIURL='http://api.forismatic.com/api/1.0/?method=getQuote&key=457653&format=json&lang=en';
+    return fetch(quoteAPIURL)
+        .then(response => response.json())
+        .then(json => {
+            return json;
+        })
+        .catch(err => {
+            bot.sendMessage(chatId, 'API goes wrong. Contact Admin');
+        });
+};
+const getWeather=(location,msg)=>{
+    const chatId=msg.chat.id;
+    let weatherobj={};
+    let arg=`select * from weather.forecast where woeid in (select woeid from geo.places(1) where text=\"${location}\") and u=\"c\"`;
+    let url=`https://query.yahooapis.com/v1/public/yql?q=${arg}&format=json`;
+    fetch(url)
+        .then(response => response.json())
+        .then(json => {
+            if (json['error']) {
+                throw json;
+            }
+            const date=(format(json.query.results.channel.item.condition.date, 'YYYY-MM-DD'));
+            const title=json.query.results.channel.title.replace('Yahoo! Weather -','');
+            weatherobj=json.query.results.channel.item.condition;
+            bot.sendMessage(chatId, `${title}\nDate: ${date}\nTemp: ${weatherobj.temp}`,{parse_mode:'HTML'});
+
+        });
+
+};
+
 if(process.env.dev=='Y'){
     const token = process.env.BOT_TOKEN;
     bot = new TelegramBot(token, {polling: true});
@@ -23,6 +59,16 @@ bot.onText(/\/start/, (msg, match) => {
     bot.sendMessage(chatId, resp);
 });
 
+bot.onText(/weather/i, (msg, match) => {
+    // 'match' is the result of executing the regexp above on the text content
+    // of the message
+
+    const chatId = msg.chat.id;
+    getWeather('taipei',msg);
+
+    //b
+
+});
 bot.onText(/hi/i, (msg,match) => {
 
     const chatId = msg.chat.id;
@@ -36,5 +82,53 @@ bot.on('sticker', sticker => {
     const chatId = sticker.chat.id;
     const stickerid=sticker.sticker.file_id;
     bot.sendSticker(chatId, stickerid);
+
 });
 
+bot.onText(/quote/i, (msg,match) => {
+    const chatId = msg.chat.id;
+    getQuote().then(rtn_json=>{
+        const quoteObj=rtn_json;
+        bot.sendMessage(chatId, "\""+quoteObj.quoteText+"\"")
+            .then(()=>{
+                bot.sendMessage(chatId, "- "+quoteObj.quoteAuthor);
+            })
+            .then(()=>{
+                const replyMarkup = JSON.stringify(
+                    {
+                        inline_keyboard:
+                            [
+                                [
+                                    {
+                                        text: 'Another Quote',
+                                        callback_data:'anotherQuote'
+                                    }
+                                ]
+                            ]
+                    }
+                );
+
+                bot.sendMessage(chatId, 'Click to get another quote!', {
+                    reply_markup: replyMarkup,
+                    parse_mode: 'markdown'
+                });
+            });
+    });
+});
+bot.on('callback_query', message => {
+    const chatId=message.message.chat.id;
+    switch (message.data)
+    {
+        case 'anotherQuote' :{
+
+            getQuote().then(rtn_json=>{
+                const quoteObj=rtn_json;
+                bot.sendMessage(chatId, "\""+quoteObj.quoteText+"\"")
+                    .then(()=>{
+                        bot.sendMessage(chatId, "- "+quoteObj.quoteAuthor);
+                    })
+            });
+            break;
+        }
+    }
+});
